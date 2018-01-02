9de2eba3d544430c0fa0cf68a62c0ded62cb5e37
Nicki Ogg <oggnicki@yahoo.com>
2018-01-02 13:41:42 -0500

    Update match for/against

--- tf2elo.py
+++ tf2elo.py
@@ -12,6 +12,18 @@ starting_elos = {
     'Ascent': 1500.0,
 }
 
+def update_elos(t1_elo, t2_elo, t1_prob, t2_prob):
+    Q1 = math.pow(10.0, t1_elo / 400.0)
+    Q2 = math.pow(10.0, t2_elo / 400.0)
+    # Calculate expected value
+    E1 = Q1 / (Q1 + Q2)
+    E2 = Q2 / (Q1 + Q2)
+
+    t1_elo = t1_elo + K * (t1_prob - E1)
+    t2_elo = t2_elo + K * (t2_prob - E2)
+    
+    return t1_elo, t2_elo
+
 def read_season(filename):
     matches = []
     with open(filename) as f:
@@ -33,6 +45,17 @@ def read_season(filename):
             if match.team1 in starting_elos:
                 team.elo = starting_elos[match.team1]
             teams[match.team1] = team
+    
+    for match in matches:
+        if not match.completed: continue
+        team1 = teams[match.team1]
+        team2 = teams[match.team2]
+        if match.winner == match.team1:
+            team1.matches_for += 1
+            team2.matches_against += 1
+        else:
+            team2.matches_for += 1
+            team1.matches_against += 1
 
     return matches, teams
 
@@ -41,53 +64,30 @@ def dump_elos(teams):
     elos = [str(team.elo) for team in sorted_teams]
     print('\t'.join(elos))
 
-def calculate_elo(matches, teams):
-    mse = 0
-    dump_elos(teams)
+def calculate_elo(matches, teams, print_changes = False):
+    if print_changes: dump_elos(teams)
     for match in matches:
         if match.completed:
             team1 = teams[match.team1]
             team2 = teams[match.team2]
-            Q1 = math.pow(10.0, team1.elo / 400.0)
-            Q2 = math.pow(10.0, team2.elo / 400.0)
-            # Calculate expected value
-            E1 = Q1 / (Q1 + Q2)
-            E2 = Q2 / (Q1 + Q2)
-
-            # Checking
-            # print(team1.name, team1.elo, team2.name, team2.elo, E1, E2)
-            # if E1 > E2 and match.t1_prob < match.t2_prob:
-            #     print(team1.name, team1.elo, team2.name, team2.elo, E1, E2)
-            #     print('Error!')
-
-            # Update mse
-            mse += math.pow(match.t1_prob - E1, 2)
-            mse += math.pow(match.t2_prob - E2, 2)
 
             # Update elo scores
-            t1_elo = team1.elo + K * (match.t1_prob - E1)
-            t2_elo = team2.elo + K * (match.t2_prob - E2)
+            t1_elo, t2_elo = update_elos(team1.elo, team2.elo, match.t1_prob, match.t2_prob)
             team1.elo = t1_elo
             team2.elo = t2_elo
             
-            dump_elos(teams)
-    return mse
+            if print_changes: dump_elos(teams)
 
 if __name__ == "__main__":
     if (len(sys.argv) != 2):
         print('Usage: python3 tf2elo.py season_file.csv')
     matches, teams = read_season(sys.argv[1])
 
-    mse = calculate_elo(matches, teams)
-    # print(str(K) + '\t' + str(mse))
-    # for k in range(10, 500):
-    #     for team in teams.values():
-    #         team.elo = 1500
-    #     K = k
-    #     mse = calculate_elo(matches, teams)
-    #     print(str(K) + '\t' + str(mse))
+    calculate_elo(matches, teams)
 
     print('=======================')
     elo_sorted = sorted(teams.values(), key = lambda team: team.elo, reverse = True)
     for team in elo_sorted:
-        print(team.name, team.elo)
+        print(team.name, team.elo, team.matches_for, team.matches_against)
+    
+    print('=======================')
